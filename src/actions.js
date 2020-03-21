fce84be717ccfe34d846aa33418624356dd67227
Daniel Laborada <jlaborada.97@gmail.com>
2020-03-21 12:54:08 +0800

    Remove unused functions.

--- src/actions.js
+++ src/actions.js
@@ -1,12 +1,10 @@
 const addTodo = string => ({ type: 'DO_ADD', payload: {details: string, ongoing: true} });
 const spliceTodo = index => ({ type: 'DO_SPLICE', payload: {tag: index} });
 const toggleTodo = index => ({ type: 'DO_TOGGLE', payload: {tag: index} });
-const getTodo = index => ({ type: 'DO_GET_TODO', payload: {tag: index} });
 
 
 export default {
     addTodo,
     spliceTodo,
     toggleTodo,
-    getTodo
   };
